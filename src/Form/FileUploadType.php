a1041ff75f68a50849863ba0b5610c080a238927
Ramon Jansen <r.b.jansen03@gmail.com>
2021-11-12 13:09:06 +0100

    terminal pushed

--- src/Form/FileUploadType.php
+++ src/Form/FileUploadType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use App\Entity\CsvFile;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Vich\UploaderBundle\Form\Type\VichFileType;
-use Symfony\UX\Dropzone\Form\DropzoneType;
+// use Symfony\UX\Dropzone\Form\DropzoneType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
