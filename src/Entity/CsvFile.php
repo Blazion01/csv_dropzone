e6a89326d7888ef3d4996d17c8be9cb9ab455b46
Ramon Jansen <r.b.jansen03@gmail.com>
2022-01-12 11:35:39 +0100

    terminal changed

--- src/Entity/CsvFile.php
+++ src/Entity/CsvFile.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\FileRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @ORM\Entity(repositoryClass=FileRepository::class)
